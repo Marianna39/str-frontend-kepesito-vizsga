7fa4cb4f5363c4b0970c463d4d87d25f8a39ddc0
Joe Cserko <j.cserko@gmail.com>
2021-06-07 09:57:20 +0200

    feat(exam): infinity scroll.

--- github-explorer/src/app/app.component.ts
+++ github-explorer/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable, fromEvent } from 'rxjs';
 import { Contributor } from './model/contributor';
 import { ContributorService } from './service/contributor.service';
@@ -8,21 +8,25 @@ import { ContributorService } from './service/contributor.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'github-explorer';
 
   eventSubscription = fromEvent(window, "scroll").subscribe(e => {
       const isBottom = document.body.offsetHeight - window.scrollY - 500 < 500;
       if (isBottom) {
         this.page++;
-        this.$list = this.contributorService.getAll(this.page);
+        this.contributorService.loadPage(this.page);
       }
   });
 
   page: number = 1;
-  $list: Observable<Contributor[]> = this.contributorService.getAll(this.page);
+  $list: Observable<Contributor[]> = this.contributorService.$list;
 
   constructor(
     private contributorService: ContributorService,
   ) {}
+
+  ngOnInit() {
+    this.contributorService.getAll(this.page).subscribe( () => {} );
+  }
 }
--- github-explorer/src/app/service/base.service.ts
+++ github-explorer/src/app/service/base.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,7 @@ export class BaseService<T> {
 
   public entity: string = '';
   public apiUrl: string = 'https://api.github.com/';
+  public $list: BehaviorSubject<T[]> = new BehaviorSubject<T[]>([]);
 
   constructor(
     public http: HttpClient
@@ -18,6 +20,18 @@ export class BaseService<T> {
   getAll(page: number): Observable<T[]> {
     return this.http.get<T[]>(
       `${this.apiUrl}${this.entity}&page=${page}`
+    ).pipe(
+      tap( (data: T[]) => this.$list.next(data) )
+    );
+  }
+
+  loadPage(page: number): void {
+    this.http.get<T[]>(
+      `${this.apiUrl}${this.entity}&page=${page}`
+    ).subscribe(
+      (data: T[]) => {
+        this.$list.next( [...this.$list.value, ...data] );
+      }
     );
   }
 }
